a1fea2e3fe74560300da01c15755234472bb8897
Drewet-freelance <41507704+Drewet-freelance@users.noreply.github.com>
2018-10-15 21:42:30 +0300

    commit

--- salsamafia/js/mail.js
+++ salsamafia/js/mail.js
@@ -1,20 +1,24 @@
 /*-----------------------------------------------	
        Send Mail
 -------------------------------------------------*/
-
 $(document).ready(function() {
-	$("#form").submit(function() {
+	$("#form").submit(function(e) {;
 		$.ajax({
 			type: "POST",
 			url: "mail.php",
 			data: $(this).serialize()
 		}).done(function() {
 			$(this).find("input").val("");
-			alert("Спасибо за заявку ! Скоро мы с вами свяжемся.");
 			$("#form").trigger("reset");
+			$("#AJAX_form").append("<div id='overlay'></div>");
+			$('#overlay').show().css({'filter' : 'alpha(opacity=90)'});
+    		e.preventDefault()
+    		$('.reg_form').show()
+		});
+		$('button.close').click(function () {
 			parent.$.fancybox.close();
+			$('#overlay').remove('#overlay');
 		});
 		return false;
 	});
 });
-
